f9750ce5516dabfdfe54ecd0b36c26382e4ba67f
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2024-07-10 00:37:34 +0200

    Stuff

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const main = () => {
   [...document.querySelectorAll("style")]
-    .filter((e) => e.textContent.match(/plone/))
+    .filter((e) => e.textContent.match(/(ploneCustom|base)/))
     .forEach((e) => e.remove());
+  const $footer = document.getElementById("sproget-footer");
+  const $elts = $footer.querySelectorAll(".footerBox");
+  const $grid = document.createElement("div");
+  $elts.forEach(($elt) => {
+    $elt.remove();
+    $grid.appendChild($elt);
+  });
+  $grid.classList.add("grid");
+  $footer.prepend($grid);
+  document.querySelector(".smallSearchButton").value = "Søg";
 };
 
-document.addEventListener("DOMContentLoaded", main);
+main();
